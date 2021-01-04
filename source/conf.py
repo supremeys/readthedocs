37be47249254b9c34a894eacfcb037a8bd07efb4
rivern.yuan <rivern.yuan@quectel.com>
2021-01-04 14:41:27 +0800

    目录结构修改

--- source/conf.py
+++ source/conf.py
@@ -66,3 +66,6 @@ extensions = ['recommonmark']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+
